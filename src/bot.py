1143a2c72d57b3db14633fb5fa1ca332f7ffa5d7
MUBASH M <42893803+MUBASH-M@users.noreply.github.com>
2025-04-13 18:27:28 +0000

    new changes

--- src/bot.py
+++ src/bot.py
@@ -1,24 +1,20 @@
 import sys
+
+# Add the workspace directory to the Python module search path
+sys.path.append('/workspaces/TechNewsBot')
+
 import os
 
-# Add the src directory to the Python path
-sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+# Set the TELEGRAM_BOT_TOKEN environment variable
+os.environ['TELEGRAM_BOT_TOKEN'] = '7827972135:AAFJiQ4BKrJ31a4SAN5Mn5UFEhGFsy_cMOE'
 
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
-from dotenv import load_dotenv
 from src.commands.start import start_command
 from src.commands.latest_news import latest_news_command
 
-# Load environment variables
-load_dotenv()
-TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
-
-if not TELEGRAM_BOT_TOKEN:
-    raise ValueError("TELEGRAM_BOT_TOKEN is not set in the environment variables.")
-
 # Initialize the bot application
-app = ApplicationBuilder().token(TELEGRAM_BOT_TOKEN).build()
+app = ApplicationBuilder().token(os.environ['TELEGRAM_BOT_TOKEN']).build()
 
 # Register command handlers
 app.add_handler(CommandHandler("start", start_command))
